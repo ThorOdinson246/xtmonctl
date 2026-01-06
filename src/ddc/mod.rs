39f5f004b73b5e5a21961c87b4a3a513840715a9
thorodinson246 <mukeshpoudel246@gmail.com>
2026-01-06 02:09:39 -0600

    chore: create package structure

---
+++ src/ddc/mod.rs
@@ -0,0 +1,4 @@
+pub mod brightness;
+pub mod detect;
+pub mod parser;
+pub mod types;
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,11 @@
+pub mod app;
+pub mod cli;
+pub mod config;
+pub mod ddc;
+pub mod error;
+pub mod tui;
+pub mod units;
+
 pub fn version() -> &'static str {
     env!("CARGO_PKG_VERSION")
 }
